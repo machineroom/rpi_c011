c8df74933b3bc2157ce4402e7cc9a38c9a1b50d0
James Wilson <macihenroomfiddling@gmail.com>
2022-03-12 17:08:31 +0000

    debug random bits set in boot return

--- boot.c
+++ boot.c
@@ -36,7 +36,13 @@ int main(int argc, char *argv[])
              0x21, 0xF8,                    /* STLF                           */
              0xF0,                          /* REV                            */
              0x60, 0x5C,                    /* LDNLP -4                       */
-             0x2A, 0x2A, 0x2A, 0x4A,        /* LDC #AAAA                      */
+             0x2A, 0x4A,                    /* LDC #AA                        */
+             0xFF,                          /* OUTWORD                        */
+             0x2B, 0x2B, 0x2B, 0x4B,        /* LDC #BBBB                      */
+             0xFF,                          /* OUTWORD                        */
+             0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x4C,             /* LDC #CCCCCC                      */
+             0xFF,                          /* OUTWORD                        */
+             0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x4D, /* LDC #DDDDDDDD                    */
              0xFF,                          /* OUTWORD                        */
              0x21, 0x2F, 0xFF              /* START                          */
              };  
@@ -44,7 +50,7 @@ int main(int argc, char *argv[])
                                    will be input from the link and placed in memory starting at MEMSTART.
                                    This code will then be executed."
                                    http://www.wizzy.com/wizzy/transputer_faq.txt*/
-    uint8_t rx[6];
+    uint8_t rx[4];
     memset(rx,0,sizeof(rx));
     uint32_t num;
     printf ("write %ld bytes to link\n",sizeof(boot)); 
@@ -55,7 +61,13 @@ int main(int argc, char *argv[])
     }
     //c011_dump_stats ("write bootstrap");
     num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
     //c011_dump_stats ("read result");
-    printf ("num read = %u, reply = [%02X %02X %02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3], rx[4], rx[5]);
+    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
 	return ret;
 }
--- c011.c
+++ c011.c
@@ -66,7 +66,7 @@ static inline void sleep_ns(int ns) {
     for (int i=0; i < ns; i++) {
         asm ("nop");
     }
-    //bcm2835_delayMicroseconds(1);
+    bcm2835_delayMicroseconds(1);
 }
 
 static void set_control_pins(void) {
