6600add7e8fa103accaee3ba8cd9b4b1a578ae21
James Wilson <macihenroomfiddling@gmail.com>
2022-03-12 17:22:18 +0000

    faffing

--- boot.c
+++ boot.c
@@ -27,24 +27,26 @@ int main(int argc, char *argv[])
     // & http://www.geekdot.com/category/software/transputer-software/ (iTest)
     uint8_t boot[] = 
             {
-             0,                             /* size of bootstrap */
-             0xB1,                          /* AJW 1, allow for Iptr store    */
-             0xD1,                          /* STL 1                          */
-             0x24, 0xF2,                    /* MINT                           */
-             0x21, 0xFC,                    /* STHF                           */
-             0x24, 0xF2,                    /* MINT                           */
-             0x21, 0xF8,                    /* STLF                           */
-             0xF0,                          /* REV                            */
-             0x60, 0x5C,                    /* LDNLP -4                       */
-             0x2A, 0x4A,                    /* LDC #AA                        */
-             0xFF,                          /* OUTWORD                        */
-             0x2B, 0x2B, 0x2B, 0x4B,        /* LDC #BBBB                      */
-             0xFF,                          /* OUTWORD                        */
-             0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x4C,             /* LDC #CCCCCC                      */
-             0xFF,                          /* OUTWORD                        */
+             0,                                         /* size of bootstrap */
+             0xB1,                                      /* AJW 1, allow for Iptr store    */
+             0xD1,                                      /* STL 1                          */
+             0x24, 0xF2,                                /* MINT                           */
+             0x21, 0xFC,                                /* STHF                           */
+             0x24, 0xF2,                                /* MINT                           */
+             0x21, 0xF8,                                /* STLF                           */
+             0xF0,                                      /* REV                            */
+             0x60, 0x5C,                                /* LDNLP -4                       */
+             0x40,                                      /* LDC #0                        */
+             0xFF,                                      /* OUTWORD                        */
+             0x2A, 0x4A,                                /* PFX A, LDC #A                        */
+             0xFF,                                      /* OUTWORD                        */
+             0x2B, 0x2B, 0x2B, 0x4B,                    /* LDC #BBBB                      */
+             0xFF,                                      /* OUTWORD                        */
+             0x2C, 0x2C, 0x2C, 0x2C, 0x2C, 0x4C,        /* LDC #CCCCCC                      */
+             0xFF,                                      /* OUTWORD                        */
              0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x2D, 0x4D, /* LDC #DDDDDDDD                    */
-             0xFF,                          /* OUTWORD                        */
-             0x21, 0x2F, 0xFF              /* START                          */
+             0xFF,                                      /* OUTWORD                        */
+             0x21, 0x2F, 0xFF                           /* START                          */
              };  
     boot[0] = sizeof (boot)-1;  /*"If the first byte received is 2 or greater then that many bytes of code 
                                    will be input from the link and placed in memory starting at MEMSTART.
@@ -61,13 +63,15 @@ int main(int argc, char *argv[])
     }
     //c011_dump_stats ("write bootstrap");
     num = c011_read_bytes (rx, sizeof(rx), 200);
-    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
-    num = c011_read_bytes (rx, sizeof(rx), 200);
-    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
-    num = c011_read_bytes (rx, sizeof(rx), 200);
-    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
-    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, expect [00 00 00 00] reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
     //c011_dump_stats ("read result");
-    printf ("num read = %u, reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, expect [AA 00 00 00] reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, expect [BB BB 00 00] reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, expect [CC CC CC 00] reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
+    num = c011_read_bytes (rx, sizeof(rx), 200);
+    printf ("num read = %u, expect [DD DD DD DD] reply = [%02X %02X %02X %02X]\n", num, rx[0], rx[1], rx[2], rx[3]);
 	return ret;
 }
